96e82fc28ca7a2f828f9ad3ec41f80a6b6df4045
LeniChabilan <leni.chabilan@etu.univ-orleans.fr>
2024-01-31 18:23:04 +0100

    oui

--- quizz/models.py
+++ quizz/models.py
@@ -6,6 +6,7 @@ class Questionnaire(db.Model):
     name = db.Column(db.String(100))
 
     def __init__(self, name):
+        super().__init__()
         self.name = name
 
     def __repr__(self):
@@ -17,6 +18,13 @@ class Questionnaire(db.Model):
             'name': self.name,
         }
         return json
+    
+
+    def get_Questionaire():
+        return Questionnaire.query.all()
+    
+    def get_Questionaire_id(id):
+        return Questionnaire.query.filter_by(id=id).first()
 
 
 class Question(db.Model):
@@ -25,3 +33,25 @@ class Question(db.Model):
     questionType = db.Column(db.String(120))
     questionnaire_id = db.Column(db.Integer, db.ForeignKey('questionnaire.id'))
     questionnaire = db.relationship("Questionnaire", backref=db.backref("questions", lazy="dynamic"))
+
+    def __init__(self, title , QuestionType, questionnaire_id):
+        super().__init__()
+        self.title = title
+        self.questionType = QuestionType
+        self.questionnaire_id = questionnaire_id
+    
+    def to_json(self):
+        json = {
+            'id': self.id,
+            'title': self.title,
+            'questionType': self.questionType,
+            'questionnaire_id': self.questionnaire_id,
+        }
+        return json
+
+    def get_Question_questionaire(id):
+        return Question.query.filter_by(questionnaire_id=id).all()
+    
+    def get_Question_id(id,id2):
+        return Question.query.filter_by(questionnaire_id=id,id=id2).first()
+    
