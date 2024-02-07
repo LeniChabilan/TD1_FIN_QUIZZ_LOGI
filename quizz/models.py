1642b3746de2a4e14b2011f7d7aacb38368aa264
LeniChabilan <leni.chabilan@etu.univ-orleans.fr>
2024-02-07 18:26:56 +0100

    ajout de presque tous les PUT sauf le dernier

--- quizz/models.py
+++ quizz/models.py
@@ -25,6 +25,18 @@ class Questionnaire(db.Model):
     
     def get_Questionaire_id(id):
         return Questionnaire.query.filter_by(id=id).first()
+    
+    def add_questionnaire(questionnaire):
+        db.session.add(Questionnaire(questionnaire))
+        db.session.commit()
+
+    def sup_questionnaire(id):
+        Questionnaire.query.filter_by(id=id).delete()
+        db.session.commit()
+    
+    def update_questionnaire(id,questionnaire):
+        Questionnaire.query.filter_by(id=id).update(questionnaire)
+        db.session.commit()
 
 
 class Question(db.Model):
@@ -33,6 +45,12 @@ class Question(db.Model):
     questionType = db.Column(db.String(120))
     questionnaire_id = db.Column(db.Integer, db.ForeignKey('questionnaire.id'))
     questionnaire = db.relationship("Questionnaire", backref=db.backref("questions", lazy="dynamic"))
+
+    # __mapper_args__ = {
+    #     'polymorphic_identity': 'question',
+    #     'with_polymorphic': '*',
+    #     'polymorphic_on': questionType
+    # }
 
     def __init__(self, title , QuestionType, questionnaire_id):
         super().__init__()
@@ -55,3 +73,35 @@ class Question(db.Model):
     def get_Question_id(id,id2):
         return Question.query.filter_by(questionnaire_id=id,id=id2).first()
     
+
+    def sup_questionnaire_question(id,id2):
+        Question.query.filter_by(questionnaire_id=id,id=id2).delete()
+        db.session.commit()
+
+    def add_question(title,questionType,questionnaire_id):
+        db.session.add(Question(title,questionType,questionnaire_id))
+        db.session.commit()
+
+    def update_question(idQuestinnaire,question):
+        Question.query.filter_by(id=id).update(question)
+        db.session.commit()
+    
+# class simpleQuestion(Question):
+#     id = db.Column(db.Integer, db.ForeignKey('question.id'), primary_key=True)
+#     # __mapper_args__ = {
+#     #     'polymorphic_identity': 'simpleQuestion',
+#     #     'with_polymorphic': '*',
+#     #     'polymorphic_on': 'inline'
+#     # }
+#     def __init__(self, title , QuestionType, questionnaire_id):
+#         super().__init__(title, QuestionType, questionnaire_id)
+
+# class multipleQuestion(Question):
+#     id = db.Column(db.Integer, db.ForeignKey('question.id'), primary_key=True)
+#     # __mapper_args__ = {
+#     #     'polymorphic_identity': 'multipleQuestion',
+#     #     'with_polymorphic': '*',
+#     #     'polymorphic_on': 'inline'
+#     # }
+#     def __init__(self, title , QuestionType, questionnaire_id):
+#         super().__init__(title, QuestionType, questionnaire_id)
